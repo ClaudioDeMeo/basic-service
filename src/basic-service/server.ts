1a16211aa795ce00c3fca344a94c4394e8e15bec
Claudio De Meo <claudio.demeo@docebo.com>
2023-01-30 12:07:03 +0100

    refactor: managed server creation with factory [skip-ci]

--- src/basic-service/server.ts
+++ src/basic-service/server.ts
@@ -1,11 +1,38 @@
 import { ServerApplication } from '../interfaces/server-application';
 import { ExpressServer } from './servers/express-server';
 
+export enum SERVER_TYPE {
+    EXPRESS = 'EXPRESS'
+}
+
+enum SERVER_METHOD {
+    EXPRESS = 'createExpressServer'
+}
+
 export abstract class Server{
 
-    private static readonly app: ServerApplication = new ExpressServer();
+    private static serverType: SERVER_TYPE;
+
+    private static app: ServerApplication;
+
+    private static createExpressServer(): ExpressServer {
+        return new ExpressServer();
+    }
+
+    public static useServer(type: SERVER_TYPE = SERVER_TYPE.EXPRESS, force?: boolean): void {
+        if (!this.app || force){
+            this.app = Server[SERVER_METHOD[type]]();
+            this.serverType = type;
+        }else if (this.serverType !== type){
+            throw Error('Server already initializated. Use \'force\' to recreate.');
+        }
+    }
 
     public static getInstance(): ServerApplication{
+        if (!this.app){
+            this.useServer(SERVER_TYPE.EXPRESS);
+        }
+
         return this.app;
     }
 
--- test/basic-service/server.test.ts
+++ test/basic-service/server.test.ts
@@ -1,14 +1,33 @@
-import { Server } from '../../src/basic-service/server';
+import { Server, SERVER_TYPE } from '../../src/basic-service/server';
 import { ExpressServer } from '../../src/basic-service/servers/express-server';
 import { ServerApplication } from '../../src/interfaces/server-application';
 
 describe('basic-service', (): void => {
     describe('Server', (): void => {
-        describe('.getInstance()', (): void => {
-            it('should return an ExpressServer object', (): void => {
+        describe('.useServer() and .getInstance()', (): void => {
+
+            beforeEach((): void => {
+                (Server as any).app = undefined;
+                (Server as any).serverType = undefined;
+            });
+
+            it('should return an ExpressServer object by default', (): void => {
                 const application: ServerApplication = Server.getInstance();
 
                 expect(application).toBeInstanceOf(ExpressServer);
+            });
+
+            it('should return an ExpressServer object after useServer is called with Express type', (): void => {
+                Server.useServer(SERVER_TYPE.EXPRESS);
+                const application: ServerApplication = Server.getInstance();
+
+                expect(application).toBeInstanceOf(ExpressServer);
+            });
+
+            it('should not throw an exception if a server is already configured with the same server type', (): void => {
+                Server.useServer(SERVER_TYPE.EXPRESS)
+
+                expect(()=>Server.useServer(SERVER_TYPE.EXPRESS)).not.toThrow();
             });
         });
     });
